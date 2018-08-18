122f7539fd3b1c230cf1c1e48013b6b9a0b8e321
Amir Zaidi <azaidi@live.nl>
2018-08-18 04:49:08 +0200

    Remove ILauncherOverlay interface and natively forward AIDL

--- app/src/main/java/amirz/aidlbridge/BridgeService.java
+++ app/src/main/java/amirz/aidlbridge/BridgeService.java
@@ -2,43 +2,24 @@ package amirz.aidlbridge;
 
 import android.app.Service;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Binder;
 import android.os.IBinder;
 import android.os.Process;
-import android.os.RemoteException;
-
-import com.google.android.libraries.launcherclient.ILauncherOverlay;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class BridgeService extends Service {
-    private static final String TAG = "BridgeService";
     private Map<Intent, Bridge> mBridges = new HashMap<>();
 
-    public BridgeService() {
-    }
-
     @Override
-    public IBinder onBind(final Intent intent) {
-        Intent forwardIntent;
-        //Intent forwardIntent = (Intent) intent.clone();
-        //forwardIntent.setPackage("com.google.android.googlequicksearchbox");
+    public IBinder onBind(Intent intent) {
+        Intent forwardIntent = (Intent) intent.clone();
 
-        String pkg = getPackageName();
-        forwardIntent = new Intent("com.android.launcher3.WINDOW_OVERLAY")
-                .setPackage("com.google.android.googlequicksearchbox")
-                .setData(Uri.parse(new StringBuilder(pkg.length() + 18)
-                        .append("app://")
-                        .append(pkg)
-                        .append(":")
-                        .append(Process.myUid())
-                        .toString())
-                        .buildUpon()
-                        .appendQueryParameter("v", Integer.toString(7))
-                        .appendQueryParameter("cv", Integer.toString(9))
-                        .build());
+        forwardIntent.setPackage("com.google.android.googlequicksearchbox");
+        forwardIntent.setData(forwardIntent.getData()
+                .buildUpon()
+                .encodedAuthority(getPackageName() + ":" + Process.myUid())
+                .build());
 
         Bridge bridge = new Bridge(getApplicationContext(), forwardIntent);
         mBridges.put(intent, bridge);
