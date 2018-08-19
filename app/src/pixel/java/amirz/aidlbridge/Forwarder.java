adb8f9d1325690bde6595ee15cb4105a5dad1adb
Amir Zaidi <azaidi@live.nl>
2018-08-19 22:51:42 +0200

    Add smartspace forwarding to Google App

--- app/src/pixel/java/amirz/aidlbridge/Forwarder.java
+++ app/src/pixel/java/amirz/aidlbridge/Forwarder.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ResolveInfo;
+import android.util.Log;
 
 public abstract class Forwarder extends BroadcastReceiver {
+    private static final String TAG = "Forwarder";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent i = (Intent) intent.clone();
@@ -18,6 +21,7 @@ public abstract class Forwarder extends BroadcastReceiver {
             if (ai != null) {
                 String packageName = ai.packageName;
                 if (packageName != null && !BuildConfig.APPLICATION_ID.equals(packageName)) {
+                    Log.w(TAG, "Sending " + i.getAction() + " to " + packageName);
                     i.setPackage(packageName);
                     context.sendBroadcast(i);
                 }
---
+++ app/src/pixel/java/amirz/aidlbridge/PixelLauncherBroadcastReceiver.java
@@ -0,0 +1,4 @@
+package amirz.aidlbridge;
+
+public class PixelLauncherBroadcastReceiver extends Forwarder {
+}
