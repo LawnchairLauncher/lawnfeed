c0ed54823dbc2c094d15157f2dca82a7f6dd70c4
Amir Zaidi <azaidi@live.nl>
2018-08-19 16:34:29 +0200

    Implement state handling with AIDL

--- app/src/main/java/amirz/aidlbridge/BridgeService.java
+++ app/src/main/java/amirz/aidlbridge/BridgeService.java
@@ -2,34 +2,52 @@ package amirz.aidlbridge;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 import android.os.Process;
+import android.util.Log;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BridgeService extends Service {
-    private Map<Intent, Bridge> mBridges = new HashMap<>();
+    private static final String TAG = "BridgeService";
+    private final Set<BridgeImpl> mBridges = new HashSet<>();
 
     @Override
     public IBinder onBind(Intent intent) {
-        Intent forwardIntent = (Intent) intent.clone();
+        Uri caller = intent.getData();
+        if (caller != null) {
+            BridgeImpl bridge = new BridgeImpl(getApplicationContext(), caller);
 
-        forwardIntent.setPackage("com.google.android.googlequicksearchbox");
-        forwardIntent.setData(forwardIntent.getData()
-                .buildUpon()
-                .encodedAuthority(getPackageName() + ":" + Process.myUid())
-                .build());
+            // Replace caller package name and UID
+            Intent forwardIntent = intent.cloneFilter();
+            forwardIntent.setPackage("com.google.android.googlequicksearchbox");
+            forwardIntent.setData(caller.buildUpon().encodedAuthority(getPackageName() + ":" + Process.myUid()).build());
 
-        Bridge bridge = new Bridge(getApplicationContext(), forwardIntent);
-        mBridges.put(intent, bridge);
-        return bridge;
+            Log.e(TAG, "Attempting bind");
+            if (bridge.bind(forwardIntent)) {
+                Log.e(TAG, "Bind success");
+                mBridges.add(bridge);
+                return bridge.getProxy();
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        mBridges.get(intent).onUnbind();
-        mBridges.remove(intent);
-        return false;
+        Uri caller = intent.getData();
+        if (caller != null) {
+            for (BridgeImpl bridge : new ArrayList<>(mBridges)) {
+                Log.e(TAG, "Attempting unbind");
+                if (bridge.unbind(caller)) {
+                    Log.e(TAG, "Unbind success");
+                    mBridges.remove(bridge);
+                }
+            }
+        }
+        return super.onUnbind(intent);
     }
 }
